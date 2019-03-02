a99860ec7b8f52bef6f4ac1f784cd72bf661e37d
Daniel Spencer <danielfspencer1@gmail.com>
2019-03-02 20:22:49 +0000

    Make buildable as electron app

---
+++ electron.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron')
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1526,
+    height: 656,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false
+    }
+  })
+  mainWindow.loadFile('index.html')
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
