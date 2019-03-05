a058b35e2e20a4521683f1b853690c56f13e0a58
Daniel Spencer <danielfspencer1@gmail.com>
2019-03-05 14:35:27 +0000

    Add update checker to about page

--- about/script.js
+++ about/script.js
@@ -1,3 +1,5 @@
+var version = "";
+
 $( document ).ready(function() {
     $( "#licence" ).click(function() {
       if (!window.chrome) {
@@ -16,4 +18,22 @@ $( document ).ready(function() {
     $( "#close" ).click(function() {
         window.close();
     });
+
+    $.getJSON("../manifest.json")
+        .done(function(data) {
+            version = data["version"]
+            $("#ver").html("Version: " + version);
+    })
+
+    $.getJSON("https://danielfspencer.github.io/blizzard-4/manifest.json")
+        .done(function(data) {
+            if (data["version"] !== version) {
+                $("#latest_ver").html("Latest Version: " + data["version"] + " (update available)");
+            } else {
+                $("#latest_ver").html("Latest Version: " + data["version"] + " (up to date)");
+            }
+        })
+        .fail( function() {
+            $("#latest_ver").html("Latest Version: (error checking for new version)");
+        })
 });
