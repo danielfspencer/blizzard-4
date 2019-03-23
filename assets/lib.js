df744cfbdf447093f03966f278e90ef17da0b422
Daniel Spencer <danielfspencer1@gmail.com>
2019-03-23 10:23:40 +0000

    Save settings when changed

--- assets/lib.js
+++ assets/lib.js
@@ -21,3 +21,34 @@ function is_chrome_app() {
         return false
     }
 }
+
+function storage_set_key(key,value) {
+    if (is_chrome_app()) {
+        var obj = {}
+        obj[key] = value
+        chrome.storage.local.set(obj)
+    } else {
+        localStorage.setItem(key,value)
+    }
+}
+
+function storage_get_key(key,callback,default_value) {
+    if (is_chrome_app()) {
+        chrome.storage.local.get([key], (items) => {
+            var result = items[key]
+            if (result === undefined) {
+                callback(default_value)
+            } else {
+                callback(result)
+            }
+        })
+
+    } else {
+        var result = localStorage.getItem(key)
+        if (result === null) {
+            callback(default_value)
+        } else {
+            callback(result)
+        }
+    }
+}
--- settings/script.js
+++ settings/script.js
@@ -1,5 +1,33 @@
 $( document ).ready(() => {
+    storage_get_key("starting-page",set_starting_page,"dem")
+    storage_get_key("theme",set_theme,"light")
+
     $( "#close" ).click(function() {
         window.close()
     })
+
+    $( "#dark-theme" ).change(function() {
+        if ($(this).prop("checked")) {
+            storage_set_key("theme","dark")
+            console.log("saving page to storage: dark")
+        } else {
+            storage_set_key("theme","light")
+            console.log("saving page to storage: light")
+        }
+    })
+
+    $( "#starting-page" ).change(function() {
+        console.log("saving page to storage: " + $(this).val())
+        storage_set_key("starting-page",$(this).val())
+    })
 })
+
+function set_theme(theme) {
+    console.log("theme from storage: " + theme)
+    $("#dark-theme").prop('checked', theme == "dark")
+}
+
+function set_starting_page(page) {
+    console.log("page from storage: " + page)
+    $("#starting-page").val(page)
+}
