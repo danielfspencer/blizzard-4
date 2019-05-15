47b81b451a28b9510325d9e13eb463dcd5b5fb26
Daniel Spencer <danielfspencer1@gmail.com>
2019-05-15 22:54:19 +0100

    Use top-frame RAM addressing for global variables

--- compiler/engine.js
+++ compiler/engine.js
@@ -829,8 +829,6 @@ function translate(token, ctx_type) {
       console.debug("global alloc '"+type+"', size '"+size+"'")
 
       var memory = alloc_global_block(size)
-      var buffer = alloc_block(size)
-      var buffer_copy = buffer.slice()
 
       var_map["[global]"][args["name"]] = memory.slice()
       name_type_map["[global]"][args["name"]] = type
@@ -838,28 +836,9 @@ function translate(token, ctx_type) {
       // make data available
       result = prefix
 
-      // copy to temp buffer
       for (var register of registers) {
-        result.push("write " + register + " ram." + buffer.shift())
+        result.push("write " + register + " ram^." + memory.shift())
       }
-
-      // need to calculate absoulute address of source
-      load_lib("sys.global.addr_calc")
-      prefix.push("write " + buffer_copy[0] + " ram+.1021")
-      prefix.push("call func_sys.global.addr_calc")
-      // absoulute address of start of buffer is now in alu.1
-
-      // addr_offset is the value of "ram#.0"
-      result.push("write 1 ctl.addrmode")
-      var addr_offset = 16384
-      for (var register of registers) {
-        result.push("write " + addr_offset + " alu.2")
-        result.push("copy [alu.+] ram#." + (memory.shift() + 15360))
-        addr_offset++
-      }
-      result.push("write 0 ctl.addrmode")
-
-      free_block(buffer_copy)
       break
 
     case "arg_alloc":       //arg [name] [type] [expr]
@@ -962,48 +941,24 @@ function translate(token, ctx_type) {
         for (var i = 0; i < dst_regs.length; i++) {
           result.push("write " + regs[i] + " ram." + dst_regs[i])
         }
+
       } else if (args["name"] in var_map["[global]"]) {
+        var dst_regs = var_map["[global]"][args["name"]]
         var dst_type = name_type_map["[global]"][args["name"]]
 
         var prefix_value_type = translate(args["expr"],dst_type)
         var prefix = prefix_value_type[0]
-        var registers = prefix_value_type[1]
+        var regs = prefix_value_type[1]
         var type = prefix_value_type[2]
 
         if (type != dst_type) {
           throw new CompError("Variable expected type '"+dst_type+"', got '"+type+"'")
         }
-
-        var size = registers.length
-
-        var memory = var_map["[global]"][args["name"]]
-        var memory_copy = memory.slice()
-        var buffer = alloc_block(size)
-        var buffer_copy = buffer.slice()
-
-        // make data available
         result = prefix
 
-        // copy to temp buffer
-        for (var register of registers) {
-          result.push("write " + register + " ram." + buffer.shift())
+        for (var i = 0; i < dst_regs.length; i++) {
+          result.push("write " + regs[i] + " ram^." + dst_regs[i])
         }
-
-        // need to calculate absoulute address of source
-        load_lib("sys.global.addr_calc")
-        prefix.push("write " + buffer_copy[0] + " ram+.1021")
-        prefix.push("call func_sys.global.addr_calc")
-        // absoulute address of start of buffer is now in alu.1
-
-        // addr_offset is the value of "ram#.0"
-        result.push("write 1 ctl.addrmode")
-        var addr_offset = 16384
-        for (var register of registers) {
-          result.push("write " + addr_offset + " alu.2")
-          result.push("copy [alu.+] ram#." + (memory_copy.shift() + 15360))
-          addr_offset++
-        }
-        result.push("write 0 ctl.addrmode")
 
       } else {
         throw new CompError("Error looking up variable '" + args["name"] + "' is undefined")
@@ -2321,26 +2276,9 @@ function translate(token, ctx_type) {
           registers.push("[" + addr + "]")
         }
       } else if (args["name"] in var_map["[global]"]) {
-        var buffer = alloc_block(var_map["[global]"][args["name"]].length)
-
-        // need to calculate absoulute address of source
-        load_lib("sys.global.addr_calc")
-        prefix.push("write " + buffer[0] + " ram+.1021")
-        prefix.push("call func_sys.global.addr_calc")
-        // absoulute address of start of buffer is now in alu.1
-
         registers = []
-        prefix.push("write 1 ctl.addrmode")
-        var addr_offset = 16384
-        for (var address of var_map["[global]"][args["name"]]) {
-          prefix.push("write " + addr_offset + " alu.2")
-          prefix.push("copy ram#." + (address + 15360) + " [alu.+]")
-          addr_offset++
-        }
-        prefix.push("write 0 ctl.addrmode")
-
-        for (var address of buffer) {
-          registers.push("[ram." + address + "]")
+        for (var addr of var_map["[global]"][args["name"]]) {
+          registers.push("[ram^."+addr+"]")
         }
 
       } else {
