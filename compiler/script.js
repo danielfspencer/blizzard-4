ab000a8fbf392017234a02371ad429df1fb2171d
Daniel Spencer <danielfspencer1@gmail.com>
2019-06-02 21:23:54 +0100

    Clean up order of functions

--- compiler/script.js
+++ compiler/script.js
@@ -1,53 +1,7 @@
-function handleMsg(data) {
-  switch(data[0]) {
-    case "update":
-      $("#progress").attr("value",data[1])
-      break
-    case "result":
-      compiling = false
-      if (assemble_when_compiled) {
-        if (data[1] !== undefined) {
-        parent.postMessage(["menu-item-asm", data[1], true, target_clock_speed],"*")
-        }
-      } else {
-        $("#out").val(data[1])
-      }
-      break
-    case "log":
-      log(data[1],data[2])
-      break
-  }
-}
-
-function compile() {
-  if (!compiling) {
-    $("#log").empty()
-    worker.postMessage(["compile",$("#in").val()])
-    compiling = true
-  }
-}
-
-function log(level,msg) {
-  var first = "<div class='item "+level+"'><img class='img' src='../assets/icons/"+level+".svg'/><src>"
-  var second = "</src></div>"
-  $("#log").append(first+msg+second)
-  $("#log").scrollTop($("#log")[0].scrollHeight - $("#log").height())  // don't do this when debug is on (too laggy), just scroll to the end
-  //~ $("#log").animate({scrollTop: $("#log")[0].scrollHeight - $("#log").height() - 1 }, 100)
-}
-
-function set_input([string, shouldAssemble, clock_speed]) {
-  document.getElementById("in").value = string
-  if (shouldAssemble) {
-  assemble_when_compiled = true;
-  target_clock_speed = clock_speed
-  }
-  compile()
-}
-
-var realtime = true
-var compiling = false
-var assemble_when_compiled = false
-var target_clock_speed = 0
+let realtime = true
+let compiling = false
+let assemble_when_compiled = false
+let target_clock_speed = 0
 
 $( document ).ready( () => {
   $(".lined-dec").linedtextarea({selectedLine: 1, dec:true})
@@ -119,3 +73,48 @@ $( document ).ready( () => {
   parent.input_data = set_input
   parent.child_page_loaded()
 })
+
+function handleMsg(data) {
+  switch(data[0]) {
+    case "update":
+      $("#progress").attr("value",data[1])
+      break
+    case "result":
+      compiling = false
+      if (assemble_when_compiled) {
+        if (data[1] !== undefined) {
+        parent.postMessage(["menu-item-asm", data[1], true, target_clock_speed],"*")
+        }
+      } else {
+        $("#out").val(data[1])
+      }
+      break
+    case "log":
+      log(data[1],data[2])
+      break
+  }
+}
+
+function compile() {
+  if (!compiling) {
+    $("#log").empty()
+    worker.postMessage(["compile",$("#in").val()])
+    compiling = true
+  }
+}
+
+function log(level,msg) {
+  var first = "<div class='item "+level+"'><img class='img' src='../assets/icons/"+level+".svg'/><src>"
+  var second = "</src></div>"
+  $("#log").append(first+msg+second)
+  $("#log").scrollTop($("#log")[0].scrollHeight - $("#log").height())
+}
+
+function set_input([string, shouldAssemble, clock_speed]) {
+  document.getElementById("in").value = string
+  if (shouldAssemble) {
+  assemble_when_compiled = true;
+  target_clock_speed = clock_speed
+  }
+  compile()
+}
