cba5788052009520f100969ccb19c81862e45360
Daniel Spencer <danielfspencer1@gmail.com>
2019-10-28 17:55:20 +0000

    Prevent update checker being dependent on network response order
    When the app is hosted as a webpage, the manifest file (containing the 
    current version) may arrive after the manifest file that contains the 
    latest version. This would cause the about page to show "version x.y.z 
    (newer version x.y.z available)"

--- about/script.js
+++ about/script.js
@@ -1,7 +1,8 @@
-var version = ""
+let version = ""
 
 $( document ).ready( () => {
   $.ajaxSetup({ cache: false }) // do not cache requests to check for latest version!
+
   $( "#licence" ).click( () => {
     windows.open("about/licence.html", 560, 800)
   })
@@ -17,24 +18,24 @@ $( document ).ready( () => {
   $( "#close" ).click(() => window.close())
 
   $.getJSON("../manifest.json")
-    .done( (data) => {
-      version = data["version_name"]
+    .done( data => {
+      version = data.version_name
       $("#version-name").html(version)
-  })
 
-  $.getJSON("https://danielfspencer.github.io/blizzard-4/manifest.json")
-    .done( (data) => {
-      var latest_version = data["version_name"]
-      if (version !== latest_version) {
-        $("#version-status").html(" (newer version " + latest_version + " available)")
-      } else {
-        $("#version-status").html(" (up to date)")
-      }
-    })
-    .fail( () => {
-      $("#version-status").html(" (error checking for new version)")
-    })
-})
+      $.getJSON("https://danielfspencer.github.io/blizzard-4/manifest.json")
+        .done( data => {
+          let latest_version = data.version_name
+          if (version !== latest_version) {
+            $("#version-status").html(` (version ${latest_version} available)`)
+          } else {
+            $("#version-status").html(" (up to date)")
+          }
+        })
+        .fail( () => {
+          $("#version-status").html(" (error checking for new version)")
+        })
+      })
+  })
 
 /* theme setting */
 
